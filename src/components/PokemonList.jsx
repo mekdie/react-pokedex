7c2666a51b2259630bbac9569a8d678664ca4306
McDony Lee <mcdonyleee@gmail.com>
2022-12-30 02:49:15 +1100

    Capitalize first letter

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 // import axios from "axios";
 const PokemonList = ({ pokemons, loading }) => {
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
     return (
         <>
             {loading ? (
@@ -14,7 +17,7 @@ const PokemonList = ({ pokemons, loading }) => {
                         return (
                             // <p key={p.id}>{p.name}</p>
                             <div key={p.id}>
-                                <li> {p.name.toUpperCase()} </li>
+                                <li> {capitalizeFirstLetter(p.name)} </li>
                                 <ul>
                                     <li>Number: #{p.number}</li>
                                     <li>
